b51c5d901bb34890bf9c524adc0454910f928b3b
paulogx77 <paulo.guilhermeez@gmail.com>
2026-09-05 23:53:15 -0300

    lab00 & 01 finshed

--- lab00-01/tb/tests/tb_baseline_test.svh
+++ lab00-01/tb/tests/tb_baseline_test.svh
@@ -9,9 +9,23 @@ class tb_baseline_test extends uvm_test;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    
+    repeat(10) 
+      `uvm_info("dbg", "build_phase override", UVM_NONE)
+
+
     env = tb_env::type_id::create("env", this);
+    uvm_top.print_topology();
   endfunction
 
+  virtual function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    
+    repeat(10)
+      `uvm_info(get_name(), "[dbg] connect_phase override", UVM_NONE)
+    
+  endfunction
+  
   virtual function void end_of_elaboration_phase(uvm_phase phase);
     super.end_of_elaboration_phase(phase);
     uvm_top.print_topology();
