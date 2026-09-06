b98499255ad98c9b3e4fdba38ca2b0f1c4cf6e9c
paulogx77 <paulo.guilhermeez@gmail.com>
2026-09-06 10:38:44 -0300

    test instance out of run_test. run_test with no parameters

--- lab00-01/tb/tb_top.sv
+++ lab00-01/tb/tb_top.sv
@@ -3,7 +3,10 @@ module tb_top;
   import uvm_pkg::*;
   `include "uvm_macros.svh"
 
+  import tb_pkg::*;
+
   initial begin
-    run_test("tb_baseline_test");
+    tb_baseline_test test = tb_baseline_test::type_id::create("test", null);
+    run_test();
   end
 endmodule
